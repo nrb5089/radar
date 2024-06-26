6791ee2faeb4446f1fb42dffd712e84a4c5e8fba
Nathan Blinn <nrb5089@gmail.com>
2024-06-25 22:21:36 -0500

    setup.py initial

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='radar',
+    version='0.1',
+    description='A Python library for learning and teach radar signal processing.',
+    author='Blinn',
+    author_email='your.email@example.com',
+    url='https://github.com/nrb5089/radar/',
+    packages=find_packages(),  # Automatically find packages in the project
+    install_requires=[
+        'numpy',
+        'scipy',
+        'opencv-python',
+    ],
+    python_requires='>=3.10',  # Specify the Python version requirement
+)
