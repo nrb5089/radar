f94c46cf1c33babcb51ee693e126b60add92f6dc
Nathan Blinn <nrb5089@gmail.com>
2024-06-25 21:01:58 -0500

    added stuff to radar to prevent error

--- radar.py
+++ radar.py
@@ -1,5 +1,15 @@
-def main():
+"""
+radar - Python library for learning and teaching radar signal processing in Software-Defined Radios (SDR).
+"""
 
+__version__ = "0.1.0"
+
+
+def main():
+	"""
+	Main function for doing stuff.
+	"""
+	
 	return 0
 	
 	
